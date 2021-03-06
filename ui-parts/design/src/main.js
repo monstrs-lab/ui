2da097b8a5d01a0f9604fb5f73ab6762790743c5
Andrey Linko <AndreyLinko@gmail.com>
2021-03-06 12:06:38 +0300

    feat(front): webpack storybook patch

--- ui-parts/design/src/main.js
+++ ui-parts/design/src/main.js
@@ -1,4 +1,28 @@
 module.exports = {
   stories: ['../../**/*.stories.@(ts|tsx|mdx)'],
   addons: ['@storybook/addon-essentials'],
+  managerWebpack: async (config) => {
+    config.module.rules.push({
+      test: /@storybook\/router\/dist\/(esm|cjs)\/utils\.js/,
+      loader: require.resolve('string-replace-loader'),
+      options: {
+        search: '/^[a-zA-Z0-9 _-]*$/',
+        replace: '/^[a-zA-Z0-9 (),_-]*$/',
+      },
+    })
+
+    return config
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /@storybook\/core-client\/dist\/(esm|cjs)\/preview\/parseArgsParam\.js/,
+      loader: require.resolve('string-replace-loader'),
+      options: {
+        search: '/^[a-zA-Z0-9 _-]*$/',
+        replace: '/^[a-zA-Z0-9 (),_-]*$/',
+      },
+    })
+
+    return config
+  },
 }

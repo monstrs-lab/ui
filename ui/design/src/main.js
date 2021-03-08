ce101b8e16dd8b51e86b005ae836c117d2a492ac
Andrey Linko <AndreyLinko@gmail.com>
2021-03-08 19:59:11 +0300

    feat(common): init composition

---
+++ ui/design/src/main.js
@@ -0,0 +1,16 @@
+module.exports = {
+  refs: {
+    parts: {
+      title: 'Parts',
+      url: 'https://ui-parts.ui.monstrs.dev',
+    },
+    proto: {
+      title: 'Proto',
+      url: 'https://ui-proto.ui.monstrs.dev',
+    },
+    admin: {
+      title: 'Admin',
+      url: 'https://ui-admin.ui.monstrs.dev',
+    },
+  },
+}

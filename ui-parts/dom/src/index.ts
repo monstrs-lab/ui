c4b29a782cee02684f3b079f85f33275e2766272
kuksi <plusaq@gmail.com>
2021-03-16 16:14:04 +0300

    feat(common): import get-content-dimensions

--- ui-parts/dom/src/index.ts
+++ ui-parts/dom/src/index.ts
@@ -1,1 +1,2 @@
 export * from './get-content-dimensions'
+export * from './convert-pixel-to-value'
--- ui-parts/scalable-content/src/use-scale.hook.ts
+++ ui-parts/scalable-content/src/use-scale.hook.ts
@@ -1,40 +1,26 @@
-import { useEffect }      from 'react'
-import { useRef }         from 'react'
-import { useState }       from 'react'
+import { useEffect }            from 'react'
+import { useRef }               from 'react'
+import { useState }             from 'react'
 
-import { UseScaleResult } from './scalable-content.interfaces'
+import { getContentDimensions } from '@ui-parts/dom'
+
+import { UseScaleResult }       from './scalable-content.interfaces'
 
 export const useScale = (): UseScaleResult => {
   const ref = useRef<HTMLDivElement>(null)
   const [scale, setScale] = useState<number>(1)
   const [mounted, setMounted] = useState<boolean>(false)
 
-  const getOffset = (parent: HTMLElement, direction: 'x' | 'y') => {
-    const style = getComputedStyle(parent, null)
-    const borderWidth = Number(style.borderWidth.replace('px', ''))
-
-    if (direction === 'x') {
-      return (Number(style.paddingLeft.replace('px', '')) + borderWidth) * 2
-    }
-    return (Number(style.paddingTop.replace('px', '')) + borderWidth) * 2
-  }
-
   useEffect(() => {
     if (ref?.current?.parentElement) {
-      const parent = ref.current.parentElement
       const childrenWidth = ref.current.offsetWidth
       const childrenHeight = ref.current.offsetHeight
-      const parentWidth = parent?.offsetWidth || 0
-      const parentHeight = parent?.offsetHeight || 0
+      const { width, height } = getContentDimensions(ref.current.parentElement)
 
-      if (parentWidth >= parentHeight) {
-        const offset = getOffset(parent, 'x')
-        setScale(parentWidth - offset < childrenWidth ? (parentWidth - offset) / childrenWidth : 1)
+      if (childrenWidth >= childrenHeight) {
+        setScale(width < childrenWidth ? width / childrenWidth : 1)
       } else {
-        const offset = getOffset(parent, 'y')
-        setScale(
-          parentHeight - offset < childrenHeight ? (parentHeight - offset) / childrenHeight : 1
-        )
+        setScale(height < childrenHeight ? height / childrenHeight : 1)
       }
 
       setMounted(true)

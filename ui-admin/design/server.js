6224fbade4a41ec14bf8987b40a60cd92124f507
Andrey Linko <AndreyLinko@gmail.com>
2024-01-03 11:57:43 +0300

    feat: design serve config

--- ui-admin/design/server.js
+++ ui-admin/design/server.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.use(express.static('static'))
+app.use(express.static('dist/static'))
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
